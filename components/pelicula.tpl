a72f424217b3619431d239d296ac091c165013eb
Andres <garzamorada@gmail.com>
2021-10-19 01:54:26 -0300

    template component

--- components/pelicula.tpl
+++
@@ -1,8 +0,0 @@
-                <figure v-for="pelicula in peliculas" class="peliculas movie-mostrar" :class=pelicula.categoria>
-                    <a :href=urlhref(pelicula.youtube,pelicula.titulo)>
-                        <img :src=urlimg(pelicula.imagen)>
-                    </a>
-                    <figcaption>
-                        {{pelicula.titulo}}
-                    </figcaption>
-                </figure>
--- js/data.js
+++ js/data.js
@@ -1,27 +1,52 @@
-document.addEventListener('DOMContentLoaded', async function() {
+    document.addEventListener('DOMContentLoaded', async function() {
 
 
-    let json = await axios('../js/peliculas.json');
-    let peliculas = json.data;
-    let html = await axios.get("../components/pelicula.tpl"); // <---- Load HTML file
-    let elemento = document.getElementById('contenedor');
-    elemento.innerHTML = html.data;
+        let json = await axios('../js/peliculas.json');
+        let peliculas = json.data;
+        let html = await axios.get("../components/pelicula.tpl.html"); // <---- Load HTML file
+        plantilla = html.data;
+        //let elemento = document.getElementById('contenedor');
+        //elemento.innerHTML = html.data;
 
-    var contenedor = new Vue({
-        el: '#contenedor',
-        data: {
-            peliculas
-        },
-        methods: {
-            urlimg(imagen) {
-                let url = "../assets/peliculas/" + imagen;
-                return url;
-            },
-            urlhref(youtube, titulo) {
-                let url = "../pages/player.html?youtube=" + youtube + "&titulo=" + titulo;
-                return url;
+
+        const movie = {
+            props: ['youtube', 'titulo', 'imagen', 'categoria'],
+            template: `${plantilla}`,
+            computed: {
+                urlhref() {
+                    return '../pages/player.html?youtube=' + this.youtube + '&titulo=' + this.titulo;
+                },
+                urlimg() {
+                    return '../assets/peliculas/' + this.imagen;
+                }
             }
+
         }
+        console.log(movie);
+
+        new Vue({
+            el: '#contenedor',
+            data: {
+                filtercategoria: "todas",
+                peliculas
+            },
+            computed: {
+                filtroCategoria() {
+                    if (this.filtercategoria != null && this.filtercategoria != '' && this.filtercategoria != 'todas') {
+                        return this.peliculas.filter(pelicula => {
+                            return pelicula.categoria === this.filtercategoria;
+                        })
+                    } else {
+                        return this.peliculas;
+                    }
+
+                }
+            },
+            components: {
+                movie,
+            }
+
+        })
 
     })
-})
+    Vue.config.devtools = true;
